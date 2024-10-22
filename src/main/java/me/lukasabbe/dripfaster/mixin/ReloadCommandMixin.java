8adc7b3acb564ca9fa03af621ab00552d1c95f7a
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-10-22 18:57:53 +0200

    1.21.2 version

--- src/main/java/me/lukasabbe/dripfaster/mixin/ReloadCommandMixin.java
+++ src/main/java/me/lukasabbe/dripfaster/mixin/ReloadCommandMixin.java
@@ -3,15 +3,21 @@ package me.lukasabbe.dripfaster.mixin;
 import com.mojang.brigadier.context.CommandContext;
 import me.lukasabbe.dripfaster.Dripfaster;
 import net.minecraft.server.command.ReloadCommand;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ReloadCommand.class)
 public class ReloadCommandMixin {
+    @Shadow @Final private static Logger LOGGER;
+
     @Inject(method = "method_13530", at=@At("HEAD"))
     private static void injectNewReloadPart(CommandContext context, CallbackInfoReturnable<Integer> cir){
+        LOGGER.info("Reloaded dripfaster");
         Dripfaster.CONFIG.reloadConfig();
     }
 }
